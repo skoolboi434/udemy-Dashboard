df654c2a7b1afc21082e66ab2e8ea7f3df516fce
Travis Davis <skoolboi434@gmail.com>
2023-11-03 16:36:29 -0400

    Backend Authentication added

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
 import connetDB from './config/db.js';
 import articleRoutes from './routes/articleRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5000;
 
@@ -13,11 +15,20 @@ connetDB(); // Connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('Api running....');
 });
 
 app.use('/api/articles', articleRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
